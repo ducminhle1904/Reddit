b27095e190e6572b86689107a2b729b5ab44714e
Minh Đức <70012356+ducminhle1904@users.noreply.github.com>
2021-09-30 20:20:46 +0700

    Update apolloClient.ts

--- client/src/lib/apolloClient.ts
+++ client/src/lib/apolloClient.ts
@@ -46,7 +46,9 @@ function createApolloClient(headers: IncomingHttpHeaders | null = null) {
   };
 
   const httpLink = new HttpLink({
-    uri: "http://localhost:4000/graphql", // Server URL (must be absolute)
+    uri: process.env.NODE_ENV === "production"
+        ? "https://lit-brushlands-74224.herokuapp.com/graphql"
+        : "http://localhost:4000/graphql", // Server URL (must be absolute)
     credentials: "include", // Additional fetch() options like `credentials` or `headers`
     fetch: enhancedFetch,
   });
